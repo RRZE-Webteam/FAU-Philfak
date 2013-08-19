d853491ef24cf096c7dd88c18f5cecd3c3ca06ac
Daniel Kestler <daniel.kestler@medienreaktor.de>
2013-08-19 16:25:16 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage FAU
+ * @since FAU 1.0
+ */
+
+/**
+ * Enqueues scripts and styles for front end.
+ *
+ * @since FAU 1.0
+ *
+ * @return void
+ */
+function fau_scripts_styles() {
+	
+	// Loads our main stylesheet.
+	wp_enqueue_style( 'fau-style', get_stylesheet_uri(), array(), '2013-08-19' );
+
+}
+add_action( 'wp_enqueue_scripts', 'fau_scripts_styles' );
+
+
+/**
+ * Creates a nicely formatted and more specific title element text for output
+ * in head of document, based on current view.
+ *
+ * @since FAU 1.0
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep Optional separator.
+ * @return string The filtered title.
+ */
+function fau_wp_title( $title, $sep ) {
+	global $paged, $page;
+
+	if ( is_feed() )
+		return $title;
+
+	// Add the site name.
+	$title .= get_bloginfo( 'name' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title = "$title $sep $site_description";
+
+	// Add a page number if necessary.
+	if ( $paged >= 2 || $page >= 2 )
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'fau' ), max( $paged, $page ) );
+
+	return $title;
+}
+add_filter( 'wp_title', 'fau_wp_title', 10, 2 );
---
+++ header.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage FAU
+ * @since FAU 1.0
+ */
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '–', true, 'right' ); ?></title>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="page" class="hfeed site">
+		<header id="masthead" class="site-header" role="banner">
+			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</a>
+
+			<div id="navbar" class="navbar">
+				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
+					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
+					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+					<?php get_search_form(); ?>
+				</nav><!-- #site-navigation -->
+			</div><!-- #navbar -->
+		</header><!-- #masthead -->
+
+		<div id="main" class="site-main">
