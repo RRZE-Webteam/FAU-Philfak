24942dce089bc52daf9ba9a3b6a9299e59b0c516
Daniel Kestler <daniel.kestler@medienreaktor.de>
2013-10-04 10:39:15 +0200

    Added 404

---
+++ 404.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage FAU
+ * @since FAU 1.0
+ */
+
+get_header(); ?>
+
+	<?php get_template_part('hero', 'small'); ?>
+
+	<div id="content">
+		<div class="container">
+		
+			<div class="row">
+				<div class="span8">
+					
+					
+				</div>
+				<div class="span4">
+
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+<?php get_footer(); ?>
