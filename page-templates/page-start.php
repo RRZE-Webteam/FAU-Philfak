2425f442cd2d782d23765bd9e04bb0a46366d829
Daniel Kestler <daniel.kestler@medienreaktor.de>
2013-10-14 17:27:16 +0200

    Bugfix

--- page-templates/page-start.php
+++ page-templates/page-start.php
@@ -105,8 +105,8 @@ get_header(); ?>
 					
 				</div>
 				<div class="span4">
-					<?php if ( is_active_sidebar( 'sidebar-front-right' ) ) : ?>
-						<?php dynamic_sidebar( 'sidebar-front-right' ); ?>
+					<?php if ( is_active_sidebar( 'sidebar-right' ) ) : ?>
+						<?php dynamic_sidebar( 'sidebar-right' ); ?>
 					<?php endif; ?>
 				</div>
 			</div>
