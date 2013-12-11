baa522c9f01a7ae1ff7cbf79c8c12f1abfeaa92a
Daniel Kestler <daniel.kestler@medienreaktor.de>
2013-12-11 14:12:40 +0100

    Changed viewport settings in meta

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
 	<title><?php wp_title( '-', true, 'right' ); ?></title>
 	<?php wp_head(); ?>
 </head>
