afe1a2aa42a91a99ca7aeef73fa7442cc7e84740
Daniel Kestler <daniel.kestler@medienreaktor.de>
2014-03-04 11:50:13 +0100

    Added portal header

--- hero-small.php
+++ hero-small.php
@@ -12,16 +12,32 @@
 <div id="hero" class="hero-small">
 	<div class="container">
 		<div class="row">
-			<div class="span8">
-
-			    <?php if(function_exists('bcn_display')): ?>
+			<div class="span12">
+				<?php if(function_exists('bcn_display')): ?>
 					<div class="breadcrumbs">
 						<a href="<?php echo esc_url( home_url( '/' ) ); ?>">Start</a><span>/</span>
 						<?php bcn_display(); ?>
 					</div>
 				<?php endif; ?>
+				
+				<div class="hero-meta-portal">
+					<?php
+						$parent = array_reverse(get_post_ancestors(get_the_ID()));
+						$first_parent = get_page($parent[0]);
+
+						if($first_parent->ID != get_the_ID()) 
+						{
+							echo $first_parent->post_title;
+						}
+					?>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="span8">
 
 				<h1><?php the_title(); ?></h1>
+				
 			</div>
 		</div>
 	</div>
