96543a5927af4f71aacba6e141adb34e88bf920e
Daniel Kestler <daniel.kestler@medienreaktor.de>
2014-04-24 16:22:50 +0200

    Smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 $(document).ready(function()
 {	
-	
+
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top - 125
+					}, 1000);
+					return false;
+				}
+			}
+		});
+
+
 	$('#header .has-sub').hover(function() {
 		$('body').addClass('flyout-toggled');
 	}, function() {
