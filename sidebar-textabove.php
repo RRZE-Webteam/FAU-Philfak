0938a2f6ff1e8af5a4e21d86a716765aebcf6da5
Daniel Kestler <daniel.kestler@medienreaktor.de>
2014-09-22 16:14:29 +0200

    Bugfix for mime type icons in sidebar

--- sidebar-textabove.php
+++ sidebar-textabove.php
@@ -3,6 +3,15 @@
 		<?php if(get_field('sidebar_title_above')): ?>
 			<h2 class="small"><?php echo get_field('sidebar_title_above'); ?></h2>
 		<?php endif; ?>
-		<?php echo get_field('sidebar_text_above'); ?>
+		<?php 
+			if(function_exists('mimetypes_to_icons')) 
+			{
+				echo mimetypes_to_icons(get_field('sidebar_text_above')); 
+			}
+			else
+			{
+				echo get_field('sidebar_text_above');
+			}
+		?>
 	</aside>
 <?php endif; ?>
--- sidebar-textbelow.php
+++ sidebar-textbelow.php
@@ -3,6 +3,15 @@
 		<?php if(get_field('sidebar_title_below')): ?>
 			<h2 class="small"><?php echo get_field('sidebar_title_below'); ?></h2>
 		<?php endif; ?>
-		<?php echo get_field('sidebar_text_below'); ?>
+		<?php 
+			if(function_exists('mimetypes_to_icons'))
+			{
+				echo mimetypes_to_icons(get_field('sidebar_text_below')); 
+			}
+			else 
+			{
+				echo get_field('sidebar_text_below');
+			}
+ 		?>
 	</aside>
 <?php endif; ?>
