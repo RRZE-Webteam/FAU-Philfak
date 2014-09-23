0fe9673f15d0385fe60787f142fdee74146e32ad
Daniel Kestler <daniel.kestler@medienreaktor.de>
2014-09-23 10:25:14 +0200

    Improved accessibility for search form

--- searchform.php
+++ searchform.php
@@ -9,6 +9,7 @@
 ?>
 
 <form role="search" method="get" class="searchform" action="<?php echo home_url( '/' )?>">
+	<label for="s"><?php _e('Searchterm','fau'); ?></label>
 	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="<?php _e('Searchterm','fau'); ?>">
 	<input type="submit" id="searchsubmit" value="<?php _e('Find','fau'); ?>">
 </form>
