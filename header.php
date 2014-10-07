b369b1ee3efd1b87b815758cdcaddbdb2d02e5ef
rvdforst <rolf.v.d.forst@googlemail.com>
2014-10-07 09:54:18 +0200

    Added favicon

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
 	<title><?php wp_title( '-', true, 'right' ); ?></title>
 	<?php wp_head(); ?>
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
 </head>
 
 <body <?php body_class(); ?>>
