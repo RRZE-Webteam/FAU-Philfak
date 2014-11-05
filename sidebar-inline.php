50f62742e5aa8cb07ed022380219a8d2133f1bd7
Wolfgang Wiese <xwolf@xwolf.de>
2014-11-05 15:46:42 +0100

    abhängigkeit zu ACF aufgehoben

--- sidebar-inline.php
+++ sidebar-inline.php
@@ -11,11 +11,16 @@
 
 ?>
 
-<?php if(get_field('sidebar_text_above') || get_field('sidebar_personen') || get_field('sidebar_quicklinks') || get_field('sidebar_quicklinks_external') || get_field('sidebar_text_below')): ?>
+<?php 
+    if ( function_exists('get_field') ) {
+	if(get_field('sidebar_text_above') || get_field('sidebar_personen') || get_field('sidebar_quicklinks') || get_field('sidebar_quicklinks_external') || get_field('sidebar_text_below')) { ?>
 	<div class="sidebar-inline">
 		<?php get_template_part('sidebar', 'textabove'); ?>
 		<?php get_template_part('sidebar', 'personen'); ?>
 		<?php get_template_part('sidebar', 'quicklinks'); ?>
 		<?php get_template_part('sidebar', 'textbelow'); ?>
 	</div>
-<?php endif; ?>
+    <?php     
+	} 
+    }
+?>
