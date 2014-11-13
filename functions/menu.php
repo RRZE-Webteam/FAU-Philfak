d29e9c534065e8477a084a0efe6480503f1dd55f
Rolf v.d. Forst <rolf.v.d.forst@googlemail.com>
2014-11-13 11:46:14 +0100

    fixed type

--- functions/menu.php
+++ functions/menu.php
@@ -125,11 +125,11 @@ class Walker_Main_Menu extends Walker_Nav_Menu
 			}
 		}
 
-		$item_output = $args['before'];
+		$item_output = $args->before;
 		$item_output .= '<a'. $attributes .'>';
-		$item_output .= $args['link_before'] . apply_filters( 'the_title', $item->title, $item->ID ) . $args['link_after'];
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
 		$item_output .= '</a>';
-		$item_output .= $args['after'];
+		$item_output .= $args->after;
 
 		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 	}
