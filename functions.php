38a1350ae8500dc79b4ea93be27337cd7e74afce
Wolfgang Wiese <xwolf@xwolf.de>
2014-11-14 16:37:29 +0100

    removed trash from header

--- functions.php
+++ functions.php
@@ -102,6 +102,18 @@ function fau_setup() {
 		
 	// This theme uses its own gallery styles.
 //	add_filter( 'use_default_gallery_style', '__return_false' );
+	
+	
+	/* Remove something out of the head */
+	remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+	remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed	remove_action( 'wp_head', 'post_comments_feed_link ', 2 ); // Display the links to the general feeds: Post and Comment Feed
+	remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+	remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+	remove_action( 'wp_head', 'index_rel_link' ); // index link
+	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+	remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+	remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+	
 }
 add_action( 'after_setup_theme', 'fau_setup' );
 
