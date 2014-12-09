801734dae91b3bd5ec521436a84d5ed617e178dd
Daniel Kestler <daniel.kestler@medienreaktor.de>
2014-12-09 11:01:30 +0100

    Changed grunt behavior, separate .min.css-files

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,14 @@ module.exports = function(grunt) {
       }
     },
     cssmin: {
- 	  options: {
-		 banner: '/*\nTheme Name: FAU\nTheme URI: www.fau.de\nAuthor: RRZE und Medienreaktor GmbH\nAuthor URI: http://www.rrze.fau.de und http://www.medienreaktor.de\nDescription: Wordpress-Theme der Startseite der Friedrich-Alexander-Universität Erlangen-Nürnberg (FAU).\nVersion: 1.1.8\nLicense: GNU General Public License v2 or later\nLicense URI: http://www.gnu.org/licenses/gpl-2.0.html\nTags: blue\nText Domain: fau\n*/\n'
-	  },
 	  build: {
-	    src: ['css/fonts.css', 'css/basis.css', 'css/mediaqueries.css', 'css/print.css'],
-	    dest: 'style.css'
+	    files: [{
+		      expand: true,
+		      cwd: 'css/',
+		      src: ['*.css', '!*.min.css'],
+		      dest: 'css/',
+		      ext: '.min.css'
+		    }]
 	  }
     }
   });
