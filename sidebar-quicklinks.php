03b2ad716efd1d64832a703acde8a9c40e047570
Wolfgang Wiese <xwolf@xwolf.de>
2015-01-20 14:06:28 +0100

    Bugfix #120 von MR

--- sidebar-quicklinks.php
+++ sidebar-quicklinks.php
@@ -6,7 +6,10 @@ if(get_field('sidebar_quicklinks') || get_field('sidebar_quicklinks_external')):
 		<?php if(get_field('sidebar_title_quicklinks')): ?>
 			<h2 class="small"><?php echo get_field('sidebar_title_quicklinks'); ?></h2>
 		<?php endif; ?>
-		<?php $quicklinks = get_field('sidebar_quicklinks'); ?>
+		<?php $quicklinks = get_field('sidebar_quicklinks'); 
+		
+		if (is_array($quicklinks)) {
+		?>
 		<ul class="tagcloud">
 			<?php foreach($quicklinks as $quicklink): ?>
 				<li class="tag"><a href="<?php echo get_permalink($quicklink->ID); ?>"><?php echo get_the_title($quicklink->ID); ?></a></li>
@@ -15,6 +18,7 @@ if(get_field('sidebar_quicklinks') || get_field('sidebar_quicklinks_external')):
 				<li class="tag"><a href="<?php the_sub_field('sidebar_quicklinks_external_link'); ?>"><?php the_sub_field('sidebar_quicklinks_external_text');?></a></li>
 			<?php endwhile; ?>
 		</ul>
+		<?php } ?>
 	</aside>
  <?php
  endif; 
