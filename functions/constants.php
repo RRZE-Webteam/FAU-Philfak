9286b9c0d4f4e4e89d65cd002991dfacebaf0293
Wolfgang Wiese <xwolf@xwolf.de>
2015-06-23 16:04:18 +0200

    Pfeil nach unten in der Bühne https://github.com/RRZE-Webteam/FAU-Philfak/issues/58

--- functions/constants.php
+++ functions/constants.php
@@ -171,7 +171,8 @@ $defaultoptions = array(
     'galery_link_original'		=> true,
 
     'advanced_post_active_subtitle'	=> true,
-
+    
+    'advanced_page_start_herojumplink'	=> false,
     'advanced_page_sidebar_titleabove'	=> true,
     'advanced_page_sidebar_titlebelow'	=> true,    
     'advanced_page_sidebar_useeditor_textabove'	=> false,
@@ -863,8 +864,7 @@ $setoptions = array(
 		  'parent'  => 'bedienung'
 		),  
 	       
-	       
-	       
+	     
 	       'advanced_post_active_subtitle'	=> array(
                   'type'    => 'bool',
                   'title'   => __( 'Untertitel (Beiträge)', 'fau' ),
@@ -878,6 +878,17 @@ $setoptions = array(
                   'type'    => 'section',
                   'title'   => __( 'Design', 'fau' ),                      
               ),
+	       
+	       
+	         'advanced_page_start_herojumplink' => array(
+                  'type'    => 'bool',
+                  'title'   => __( 'Sprunglink unter der Bühne', 'fau' ),
+                  'label'   => __( 'Aktiviert die Schaltung eines Sprunglinks unterhalb der Bühne der Startseite, wenn das Browserfenster eine Größe zwischen 700px und 900px Höhe hat.', 'fau' ),                
+                  'default' => $defaultoptions['advanced_page_start_herojumplink'],
+		  'parent'  => 'design'
+		),  
+
+	       
 	       
 	      'galery_link_original'	  => array(
                   'type'    => 'bool',
